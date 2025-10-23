0aed1fc23ccf08f4e9cd244e0c0ee1ad84bead2f
Justin Priest <justin.priest@alaska.gov>
2025-10-23 14:58:04 -0800

    Went through code and compared to theme_gray() defaults. Flagged all lines that were redundant. Recommend deletion after review with team

--- R/theme_adfg.R
+++ R/theme_adfg.R
@@ -8,6 +8,7 @@
 #' @param font_size Size in points for font. Default is 18.
 #' @param font_family Sets which font family you want to use. Default is
 #' Times New Roman.
+#' @param box TRUE/FALSE selection for whether to outline the plot in a box
 #' @param ... Other arguments that are standard to ggplot \code{theme()}
 #'
 #' @return The output returned will be either an object of \code{theme_adfg()},
@@ -39,8 +40,10 @@ theme_adfg = function(font_size = 18,
                        #               in regular ggplot so we don't need to add it here I don't think.
                        #strip.placement = "outside", #where labels are placed relative to the plot panels (ex: "outside"; "inside")
                        # JTP comments: Same comments as above, rename or exclude.
-                       strip.text.y = element_text(angle = -90), #how the y-axis are drawn. Here, it is at a 90 degree angle
+                       #strip.text.y = element_text(angle = -90), #how the y-axis are drawn. Here, it is at a 90 degree angle
+                       # JTP: the strip.text.y was already defaulted to -90. It can still be modified.
                        box = TRUE, #if box = TRUE, black rectangle border
+                       base_line_size = font_size / 22, #JTP added based on code from ggplot
                        # JTP comments: Changed box to TRUE rather than "yes". Use T/F instead so that "Yes" or "YES" doesn't confuse
                       ...
 )
@@ -76,34 +79,32 @@ theme_adfg = function(font_size = 18,
       line = element_line(
         color = "black",
        # size = line_size, obsolete, use linewidth instead
-        linetype = 1,
-        lineend = "butt"
+        linewidth = base_line_size,
+        #linetype = 1,
+        #lineend = "butt" # FLAG for deletion. Redundant to theme_gray()
       ),
       rect = element_rect(
-        fill = NA,
-        color = NA,
-        #size = line_size, depreciated
-        linetype = 1
+        fill = NA, color = NA,
+        linewidth = base_line_size,
       ),
       text = element_text(
         family = font_family,
-        face = "plain",
         color = "black",
         size = font_size,
-        hjust = 0.5,
-        vjust = 0.5,
-        angle = 0,
-        lineheight = 0.9,
-        margin = margin(),
-        debug = FALSE
+        #hjust = 0.5, # FLAG for deletion. All the following are redundant to theme_gray()
+        #vjust = 0.5,
+        #angle = 0,
+        #lineheight = 0.9,
+        #margin = margin(),
+        #debug = FALSE
       ),
       axis.line = element_line(
         color = "black",
         #size = line_size, #agr turned off
         lineend = "square"
       ),
-      axis.line.x = NULL,
-      axis.line.y = NULL,
+      #axis.line.x = NULL, # FLAG for deletion. All the following are redundant to theme_gray()
+      #axis.line.y = NULL, # FLAG for deletion. All the following are redundant to theme_gray()
       axis.text = element_text(color = "black",
                                size = small_size),
       axis.text.x = element_text(margin = margin(t = small_size / 4),
@@ -117,8 +118,8 @@ theme_adfg = function(font_size = 18,
       axis.ticks = element_line(color = "black",
                                 #size = line_size
                                 ),
-      axis.ticks.length = unit(qtr_line, "pt"),
-      axis.title.x = element_text(margin = margin(t = qtr_line), vjust = -0.75),
+      axis.ticks.length = unit(qtr_line, "pt"), #JTP: explore how this is different than default rel(0.5)
+      axis.title.x = element_text(margin = margin(t = qtr_line), vjust = -0.75), #default vjust = 1
       axis.title.x.top = element_text(margin = margin(b = qtr_line), vjust = -0.75),
       axis.title.y = element_text(
         angle = 90,
@@ -126,19 +127,19 @@ theme_adfg = function(font_size = 18,
         vjust = 3
       ),
       axis.title.y.right = element_text(
-        angle = -90,
+        #angle = -90, # FLAG for deletion. redundant to theme_gray()
         margin = margin(l = qtr_line),
-        vjust = 0
+        vjust = 0 # default vjust = 1
       ),
-      legend.background = element_blank(),
-      legend.spacing = unit(font_size, "pt"),
-      legend.spacing.x = NULL,
-      legend.spacing.y = NULL,
+      legend.background = element_blank(), # I think that this does nothing
+      legend.spacing = unit(font_size, "pt"), # default rel(2)
+      #legend.spacing.x = NULL, # FLAG for deletion. redundant to theme_gray()
+      #legend.spacing.y = NULL, # FLAG for deletion. redundant to theme_gray()
       legend.margin = margin(0, 0, 0, 0),
-      legend.key = element_blank(),
+      legend.key = element_blank(), # default is NULL
       legend.key.size = unit(1.1 * font_size, "pt"),
-      legend.key.height = NULL,
-      legend.key.width = NULL,
+      #legend.key.height = NULL, # FLAG for deletion. redundant to theme_gray()
+      #legend.key.width = NULL, # FLAG for deletion. redundant to theme_gray()
       legend.text = element_text(size = rel(rel_small)),
       legend.text.align = NULL,
       legend.title = element_text(hjust = 0), #edit this to correct for the legend.title.align depreciating, if needed
@@ -148,12 +149,12 @@ theme_adfg = function(font_size = 18,
       #legend.position.inside = legend.position, #correcting for depreciation; moving this elsewhere
       legend.direction = NULL,
       #legend.justification = legend.justification, # JTP: I turned this off. It changes nothing
-      legend.box = NULL,
+      #legend.box = NULL, # FLAG for deletion. redundant to theme_gray()
       legend.box.margin = margin(0, 0, 0, 0),
-      legend.box.background = element_blank(),
+      #legend.box.background = element_blank(), # FLAG for deletion. redundant to theme_gray()
       legend.box.spacing = unit(font_size, "pt"),
       panel.background = element_blank(),
-      panel.border = element_blank(),
+      #panel.border = element_blank(), # FLAG for deletion. redundant to theme_gray()
       panel.grid = element_blank(),
       panel.grid.major = NULL,
       panel.grid.minor = NULL,
@@ -162,19 +163,19 @@ theme_adfg = function(font_size = 18,
       panel.grid.minor.x = NULL,
       panel.grid.minor.y = NULL,
       panel.spacing = unit(half_line, "pt"),
-      panel.spacing.x = NULL,
-      panel.spacing.y = NULL,
-      panel.ontop = FALSE,
+      #panel.spacing.x = NULL, # FLAG for deletion. redundant to theme_gray()
+      #panel.spacing.y = NULL, # FLAG for deletion. redundant to theme_gray()
+      #panel.ontop = FALSE, # FLAG for deletion. redundant to theme_gray()
       # strip.background = strip.background, idk what this was doing AGR
       strip.text = element_text(
         size = rel(rel_small),
         margin = margin(qtr_line, qtr_line, qtr_line, qtr_line)
       ),
-      strip.text.x = NULL,
-      strip.text.y = strip.text.y,
+      #strip.text.x = NULL, # FLAG for deletion. redundant to theme_gray()
+      #strip.text.y = strip.text.y,
       #strip.placement = strip.placement,# changed from "inside"
-      strip.placement.x = NULL,
-      strip.placement.y = NULL,
+      #strip.placement.x = NULL, # FLAG for deletion. redundant to theme_gray()
+      #strip.placement.y = NULL, # FLAG for deletion. redundant to theme_gray()
       strip.switch.pad.grid = unit(qtr_line, "pt"),
       strip.switch.pad.wrap = unit(qtr_line, "pt"),
       #plot.background = element_blank(),
@@ -182,30 +183,24 @@ theme_adfg = function(font_size = 18,
       plot.title = element_text(
         face = "bold",
         size = rel(rel_large),
-        hjust = 0,
-        vjust = 1,
-        margin = margin(b = half_line)
+        margin = margin(b = half_line) # I think this changes nothing as it's the default but not sure
       ),
       plot.subtitle = element_text(
         size = rel(rel_small),
-        hjust = 0,
-        vjust = 1,
-        margin = margin(b = half_line)
+        margin = margin(b = half_line) # I think this changes nothing as it's the default but not sure
       ),
       plot.caption = element_text(
         size = rel(rel_tiny),
-        hjust = 1,
-        vjust = 1,
-        margin = margin(t = half_line)
+        margin = margin(t = half_line) # I think this changes nothing as it's the default but not sure
       ),
       plot.tag = element_text(
         face = "bold",
         hjust = 0,
-        vjust = 0.7
+        vjust = 0.7 #defaults for this are 0.5, 0.5
       ),
-      plot.tag.position = c(0, 1),
-      plot.margin = margin(20, 20, 20, 25), #was half_line x 4
-      complete = TRUE
+      plot.tag.position = c(0, 1), # default is topleft which I believe is the exact same as this
+      plot.margin = margin(20, 20, 20, 25) #was half_line x 4
+      #complete = TRUE # FLAG for deletion. redundant to theme_gray()
       #legend.position = "inside",
       #legend.position.inside = legend.position.set #agr changed
     )
